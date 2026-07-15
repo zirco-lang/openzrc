981bce6246d8400b4f7ed9b0f7870e22f4733268
Cory Borek <agentdid127cb@aol.com>
2026-07-15 11:09:05 -0400

    Read a zirco file

--- stage1.c
+++ stage1.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
-int main(int argc, char* argv[]) {
-	printf("This is a test!\n");
+
+//====================
+// Main Program
+//====================
+
+/*
+ * This is our main program function
+ */
+int main(int argc, char ** argv) {
+  printf("openzrc stage 1 compiler\n");
+
+
+  // read a zirco file.
+  char * file = "./example.zr";
+  if (argc > 1) {
+    file = argv[1];
+  }
+  FILE * fptr;
+  fptr = fopen(file, "r");
+  if (fptr == NULL) {
+    printf("Error reading file: %s\n", file);
+    return 1;
+  }
+
+  fclose(fptr);
+  return 0;
 }
