b95faadf02fcfcd5b80448d52302be33858708bf
Cory Borek <agentdid127cb@aol.com>
2026-07-15 11:20:12 -0400

    Code generation first pass

--- stage1.c
+++ stage1.c
@@ -1,3 +1,10 @@
+
+#include <llvm-c/Core.h>
+#include <llvm-c/ExecutionEngine.h>
+#include <llvm-c/Target.h>
+#include <llvm-c/Analysis.h>
+#include <llvm-c/BitWriter.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -436,12 +443,230 @@ int parser(TOKEN ** tokens, int alloc_tokens, GRAMMAR_T * out) {
   return idx;
 }
 
+//===================
+// Code Generation
+//===================
+
+
+/*
+ * Value list, stores a reference value for variables in a linked list
+ */
+typedef struct value_list {
+  char name[64];
+  LLVMValueRef val;
+  struct value_list * next;
+} VALUE_LIST;
+
+/*
+ * Adds a reference value to the linked list.
+ */
+void add_value(char * name, LLVMValueRef val, VALUE_LIST ** vl) {
+  VALUE_LIST * current;
+  if (*vl == 0) {
+    *vl = malloc(sizeof(VALUE_LIST));
+    current = *vl;
+  } else {
+    current = *vl;
+    while (current->next != 0) {
+      current = current->next;
+    }
+
+    current->next = malloc(sizeof(VALUE_LIST));
+    current = current->next;
   }
+  current->next = 0;
+  strcpy(current->name, name);
+  memcpy(&current->val, &val, sizeof(LLVMValueRef));
+}
+
+/*
+ * Frees a value linked list
+ */
+void free_list(VALUE_LIST** vl) {
+  if (*vl == 0) return;
+  VALUE_LIST * current = *vl;
+  VALUE_LIST * prev = 0;
+  while ((*vl)->next != 0) {
+    current = *vl;
+    prev = 0;
+    while (current->next != 0) {
+      prev = current;
+      current = current->next;
+    }
+    free(current);
+    if (prev != 0) prev->next = 0;
   }
+  free(*vl);
+  
+}
+
+/*
+ * Gets a value from the value linked list.
+ */
+int get_value(char * name, VALUE_LIST ** vl, LLVMValueRef * out) {
+  VALUE_LIST * current = *vl;
+  while (current != 0 && (strcmp(current->name, name) != 0)) {
+    current = current->next;
   }
+
+  if (current == 0) {
+    return 0;
   }
+  if (strcmp(current->name, name) == 0) {
+     *out = current->val;
+  }
+  return 0;
+}
+
+
+/*
+ * Gets a LLVMTypeRef from a token
+ */
+void get_type_ref(LLVMTypeRef * ref, int tok) {
+  switch (tok) {
+    
   case TOK_I32:
   default:
+    *ref = LLVMInt32Type();
+    break;
+  }
+}
+
+
+/*
+ * Generates an expression.
+ */
+int gen_expr(GRAMMAR_T * parse_tree, LLVMModuleRef* mod, LLVMBuilderRef * builder, VALUE_LIST** vl, LLVMValueRef * out, int i) {
+  switch (parse_tree->typ) {
+  case PARSER_TOKEN:
+    {
+      TOKEN * tok = (TOKEN*)parse_tree->val;
+      switch (tok->tok) {
+      case TOK_LITERAL:
+	{
+	  *out = LLVMConstInt(LLVMInt32Type(), atoi(tok->val), 0);
+	}
+	break;
+	// TODO: variables
+      }
+      
+    }
+    break;
+  case PARSER_UNARY_OP:
+    {
+      UNARY_OP * uop = (UNARY_OP*) parse_tree->val;
+      LLVMValueRef lhs;
+      LLVMValueRef rhs;
+      gen_expr(uop->lhs, mod, builder, vl, &lhs, i+1);
+      gen_expr(uop->rhs, mod, builder, vl, &rhs, i+1);
+
+      TOKEN * typ = uop->typ;
+
+      switch (typ->tok) {
+      case TOK_PLUS:
+	{
+	  char name[32];
+	  snprintf(name, 32, "_add_%d", i);
+	  *out = LLVMBuildAdd(*builder, lhs, rhs, name);
+	}
+	break;
+      }
+      
+    }
+    break;
+  }
+  return 0;
+}
+
+/*
+ * Generates a statement
+ */
+int gen_stmt(GRAMMAR_T * parse_tree, LLVMModuleRef* mod, LLVMBuilderRef * builder, LLVMValueRef * fn, VALUE_LIST** vl, int idx) {
+  
+  switch (parse_tree->typ) {
+  case PARSER_LET_DECL:
+    {
+      LET_DECL * decl = (LET_DECL*)parse_tree->val;
+      char * name = decl->identifier->val;
+      int typ = decl->typ->tok;
+      LLVMTypeRef var_typ;
+      get_type_ref(&var_typ, typ);
+
+      add_value(name, LLVMBuildAlloca(*builder, var_typ, name), vl);
+      if (decl->val != 0) {
+	LLVMValueRef ref;
+	LLVMValueRef get;
+	gen_expr(decl->val, mod, builder, vl, &ref, 0);
+	get_value(name, vl, &get);
+	LLVMBuildStore(*builder, ref, get);
+      }   
+    }
+    break;
+  }
+  return 0;
+}
+
+/*
+ * This is for early development, where no function definitions exist. This generates a test function.
+ */
+int test_gen_fn(GRAMMAR_T * parse_tree, LLVMModuleRef* mod, LLVMBuilderRef * builder, VALUE_LIST ** vl) {
+
+  // No parameters, returns an integer
+  LLVMTypeRef param_types[] = {};
+  LLVMTypeRef ret_type = LLVMFunctionType(LLVMInt32Type(), param_types, 0, 0);
+
+  // Main function name
+  LLVMValueRef main = LLVMAddFunction(*mod, "main", ret_type);
+  // Entry point
+  LLVMBasicBlockRef entry = LLVMAppendBasicBlock(main, "entry");
+  LLVMPositionBuilderAtEnd(*builder, entry);
+  
+  // Generates a single statement from the parse tree
+  gen_stmt((GRAMMAR_T*)parse_tree->val, mod, builder, &main, vl, 0);
+
+  // makes the function return
+  LLVMBuildRet(*builder, LLVMConstInt(LLVMInt32Type(), 0, 0));
+  
+  return 0;
+}
+
+
+/*
+ * Generates the final file.
+ */
+int gen_code(char * input_name, char * output_name, GRAMMAR_T * parse_tree) {
+
+  // Create module and builder
+  LLVMModuleRef mod = LLVMModuleCreateWithName(input_name);
+  LLVMBuilderRef builder = LLVMCreateBuilder();
+
+  // Value linked list
+  VALUE_LIST * vl = 0;
+
+
+  // TODO: do real function generation, for now test function
+  test_gen_fn(parse_tree, &mod, &builder, &vl);
+  
+
+  // Compile
+  char *error = NULL;
+  LLVMVerifyModule(mod, LLVMAbortProcessAction, &error);
+  LLVMDisposeMessage(error);
+  
+  LLVMInitializeNativeTarget();
+  LLVMInitializeNativeAsmPrinter();
+
+  if (LLVMWriteBitcodeToFile(mod, output_name) != 0) {
+    fprintf(stderr, "error writing bitcode to file, skipping\n");
+  }
+
+  // Clean up
+  LLVMDisposeBuilder(builder);
+  free_list(&vl);
+  
+  return 0;
+}
+
 //====================
 // Main Program
 //====================
@@ -473,6 +698,25 @@ int main(int argc, char ** argv) {
   int num_tokens = lexer(fptr, &tokens, alloc_tokens);
   fclose(fptr);
 
+  // set up the parser
+  GRAMMAR_T * parse_tree = malloc(sizeof(GRAMMAR_T));
+  (*parse_tree).typ = PARSER_UNKNOWN;
+
+  // run the parser
+  int parsed = parser(&tokens, alloc_tokens, parse_tree);
+  if (parsed < 0) {
+    printf("unable to parse information properly\n");
+  }
+  
+  // Print the parse tree
+  printf("Parse tree:\n");
+  print_tree(parse_tree);
+
+  // start to generate some basic code
+  gen_code(file, "output.bc", parse_tree);
+
+  // free the parser and lexer since we are good.
+  free_parser(parse_tree);
   for (int i = 0; i < num_tokens; i++) {
     free(tokens[i].val);
   }
